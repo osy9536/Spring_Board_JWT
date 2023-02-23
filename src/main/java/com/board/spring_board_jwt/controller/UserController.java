2a97ee12d90cdca5f54a1edb5d7798a0c3863664
Seyoung Oh <osy9536@naver.com>
2023-02-23 12:09:20 +0900

    refactor : 특정 로직 AOP 제외처리
    annotation을 만들어 특정 메서드에서 커스텀 annotation을 적용해 AOP가 작동하지 않게 처리

--- src/main/java/com/board/spring_board_jwt/controller/UserController.java
+++ src/main/java/com/board/spring_board_jwt/controller/UserController.java
@@ -1,33 +1,34 @@
 package com.board.spring_board_jwt.controller;
 
+import com.board.spring_board_jwt.annotation.NoLogging;
+import com.board.spring_board_jwt.dto.ResponseMsgDto;
 import com.board.spring_board_jwt.dto.UserRequestDto;
+import com.board.spring_board_jwt.security.UserDetailsImpl;
 import com.board.spring_board_jwt.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity signup(@Valid @RequestBody UserRequestDto userRequestDto) {
+    public ResponseMsgDto signup(@Valid @RequestBody UserRequestDto userRequestDto) {
         return userService.signup(userRequestDto);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody UserRequestDto userRequestDto, HttpServletResponse response) {
+    public ResponseMsgDto login(@RequestBody UserRequestDto userRequestDto, HttpServletResponse response) {
         return userService.login(userRequestDto, response);
     }
-
     @PostMapping("/withdrawal")
-    public ResponseEntity<Object> withdrawal(@RequestBody UserRequestDto userRequestDto, HttpServletResponse response) {
-        return userService.withdrawal (userRequestDto, response);
+    public ResponseMsgDto withdrawal(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return userService.withdrawal (userDetails.getUser());
     }
 }
