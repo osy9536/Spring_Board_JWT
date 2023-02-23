69efa18e6c757aa2891537430819a8c4a6536a71
Seyoung Oh <osy9536@naver.com>
2023-02-23 12:21:28 +0900

    feat : 특정 로직 AOP 제외처리
    annotation을 만들어 특정 메서드에서 커스텀 annotation을 적용해 AOP가 작동하지 않게 처리

--- src/main/java/com/board/spring_board_jwt/aop/WithdrawalAop.java
+++ src/main/java/com/board/spring_board_jwt/aop/WithdrawalAop.java
@@ -4,15 +4,13 @@ import com.board.spring_board_jwt.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class WithdrawalAop {
-    @Before("execution(* com..service..*(..)) && args(user, ..)")
+    @Before("execution(* com..service..*(..)) && !@annotation(com.board.spring_board_jwt.annotation.NoChecking) && args(user, ..)") //NoLogging 어노테이션이 붙은 메서드 제외
     public void beforeMethod(User user) {
         if (user.isWithdrawal()) {
             throw new IllegalArgumentException("탈퇴된 회원입니다.");
--- src/main/java/com/board/spring_board_jwt/service/UserService.java
+++ src/main/java/com/board/spring_board_jwt/service/UserService.java
@@ -1,5 +1,6 @@
 package com.board.spring_board_jwt.service;
 
+import com.board.spring_board_jwt.annotation.NoChecking;
 import com.board.spring_board_jwt.dto.ResponseMsgDto;
 import com.board.spring_board_jwt.dto.UserRequestDto;
 import com.board.spring_board_jwt.entity.User;
@@ -25,18 +26,13 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public ResponseEntity<Object> signup(UserRequestDto userRequestDto) {
+    public ResponseMsgDto signup(UserRequestDto userRequestDto) {
         String username = userRequestDto.getUsername();
         String password = passwordEncoder.encode(userRequestDto.getPassword());
-        //중복 회원 에러
-        ResponseMsgDto sameUser = ResponseMsgDto.builder()
-                .msg("중복된 username입니다.")
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .build();
         // 회원 중복 확인
         Optional<User> found = userRepository.findByUsername(username);
         if (found.isPresent()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sameUser);
+            throw new IllegalArgumentException("중복된 username입니다.");
         }
         UserRoleEnum role = UserRoleEnum.USER;
         if (userRequestDto.isAdmin()) {
@@ -52,72 +48,53 @@ public class UserService {
                 .role(role)
                 .build();
         userRepository.save(user);
-        ResponseMsgDto ok = ResponseMsgDto.builder()
+        return ResponseMsgDto.builder()
                 .msg("회원가입 완료!")
                 .statusCode(HttpStatus.OK.value())
                 .build();
-        return ResponseEntity.status(HttpStatus.OK).body(ok);
     }
 
 
     @Transactional
-    public ResponseEntity<Object> login(UserRequestDto userRequestDto, HttpServletResponse response) {
+    public ResponseMsgDto login(UserRequestDto userRequestDto, HttpServletResponse response) {
         String username = userRequestDto.getUsername();
         String password = userRequestDto.getPassword();
-        ResponseMsgDto noUser = ResponseMsgDto.builder()
-                .msg("회원을 찾을 수 없습니다.")
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .build();
-        ResponseMsgDto withdrawUser = ResponseMsgDto.builder()
-                .msg("탈퇴한 회원입니다.")
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .build();
         if (userRepository.findByUsername(username).isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(noUser);
+            throw new IllegalArgumentException("회원을 찾을 수 없습니다.");
         }
         User user = userRepository.findByUsername(username).get();
         if (user.isWithdrawal()) { //탈퇴 회원 검증
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(withdrawUser);
+            throw new IllegalArgumentException("탈퇴한 회원입니다.");
         }
         if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
-        ResponseMsgDto responseMsgDto = ResponseMsgDto.builder()
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername(), user.getRole()));
+        return ResponseMsgDto.builder()
                 .msg("로그인 완료!")
                 .statusCode(HttpStatus.OK.value())
                 .build();
-        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername(), user.getRole()));
-        return ResponseEntity.status(HttpStatus.OK).body(responseMsgDto);
     }
-    @Transactional
-    public ResponseEntity<Object> withdrawal(UserRequestDto userRequestDto, HttpServletResponse response) {
-        String username = userRequestDto.getUsername();
-        String password = userRequestDto.getPassword();
-        ResponseMsgDto noUser = ResponseMsgDto.builder()
-                .msg("회원을 찾을 수 없습니다.")
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .build();
 
-        if (userRepository.findByUsername(username).isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(noUser);
-        }
-        User user = userRepository.findByUsername(username).get();
-        if (!passwordEncoder.matches(password, user.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-        ResponseMsgDto responseMsgDto = ResponseMsgDto.builder()
-                .msg("계정 탈퇴 성공")
-                .statusCode(HttpStatus.OK.value())
-                .build();
-        ResponseMsgDto responseMsgDto2 = ResponseMsgDto.builder()
-                .msg("계정 복구 성공")
-                .statusCode(HttpStatus.OK.value())
-                .build();
-        user.changeWithdrawal();
-        if (user.isWithdrawal()) {
-            return ResponseEntity.status(HttpStatus.OK).body(responseMsgDto);
+    @NoChecking
+    @Transactional
+    public ResponseMsgDto withdrawal(User user) {
+        User user1 = userRepository.findByUsername(user.getUsername()).orElseThrow(
+                () -> {
+                    throw new IllegalArgumentException("회원이 존재하지 않습니다.");
+                }
+        );
+        user1.changeWithdrawal();
+        if (user1.isWithdrawal()) {
+            return ResponseMsgDto.builder()
+                    .msg("계정 탈퇴 성공")
+                    .statusCode(HttpStatus.OK.value())
+                    .build();
         } else {
-            return ResponseEntity.status(HttpStatus.OK).body(responseMsgDto2);
+            return ResponseMsgDto.builder()
+                    .msg("계정 복구 성공")
+                    .statusCode(HttpStatus.OK.value())
+                    .build();
         }
     }
 }
